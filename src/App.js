91a39ed30ce706ebd8db7f6df2ec982eef5dea90
Vagelis P <hello@vagelis.dev>
2021-08-09 14:21:14 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Posts from "./Posts";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Posts</h1>
+      <Posts />
+    </div>
+  );
+}
